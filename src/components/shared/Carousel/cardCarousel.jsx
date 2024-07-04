b7a37bdb7dabd3d6262e708475cfbe2a1084ad8a
Nuta Shukla <nuta@codnestx.com>
2024-07-04 19:11:44 +0530

    banner changes

--- src/components/shared/Carousel/cardCarousel.jsx
+++ src/components/shared/Carousel/cardCarousel.jsx
@@ -1,19 +1,10 @@
 import React, { memo } from 'react';
-import { Card } from '@mui/material';
 import { Carousel } from 'primereact/carousel';
 import CarouselItem from './carouselItem';
 import './cardCarousel.css';
 import CardBg from './cardbg.png';
 
 const CardCarousel = ({ BannerData }) => {
-  const items = [
-    { imageUrl: 'https://via.placeholder.com/300x150' },
-    { imageUrl: 'https://via.placeholder.com/300x150' },
-    { imageUrl: 'https://via.placeholder.com/300x150' },
-    { imageUrl: 'https://via.placeholder.com/300x150' },
-    { imageUrl: 'https://via.placeholder.com/300x150' },
-  ];
-
   const responsiveOptions = [
     {
       breakpoint: '1400px',
@@ -38,28 +29,28 @@ const CardCarousel = ({ BannerData }) => {
   ];
 
   const itemTemplate = item => {
-    console.log('🚀 ~ CardCarousel ~ item:', item);
     return (
-      <Card className='w-96 ml-4'>
-        <div className='App bg-darkBlue flex items-center justify-center h-60'>
+      <div className='w-full px-2'>
+        <div className='bg-darkBlue rounded-xl flex items-center justify-center h-60'>
           <CarouselItem
             backgroundImage={CardBg}
             image={item?.image?.url}
             item={item}
           />
         </div>
-      </Card>
+      </div>
     );
   };
 
   return (
-    <div className='relative mb-10 mt-8 w-full card pr-0'>
+    <div className='relative mb-10 mt-8 w-full overflow-hidden'>
       <Carousel
         value={BannerData}
         numVisible={3}
         numScroll={1}
         responsiveOptions={responsiveOptions}
         itemTemplate={itemTemplate}
+        className='w-full'
       />
     </div>
   );
